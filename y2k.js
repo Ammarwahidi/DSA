4be3de336655881fe13b13795a76ca4f5381b3bd
Ammarwahidi <wahidiammar@gmail.com>
2022-12-29 20:40:44 +0530

    first commit

---
+++ y2k.js
@@ -0,0 +1,9 @@
+var num = parseInt(window.prompt("Enter a value"));
+console.log(yearcheck(num));
+function yearcheck(num) {
+  if (num < 35) {
+    return num + 2000;
+  } else {
+    return num + 1900;
+  }
+}
